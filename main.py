d1e624019c6329f346a3ae2938dd4992be9b92dc
rozenko voitenko v.a. (vr4n18) <vr4n18@soton.ac.uk>
2019-02-25 19:45:01 +0000

    working animation example in main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # STANDARD LIBRARY IMPORTS
-from Tkinter import *
-from time import sleep as delay
+import Tkinter
+from time import sleep
 
-# OWN MODULES IMPORTS 
+# OWN MODULES IMPORTS
 from lib.bird import Bird
 from lib.obstacle import Obstacle
 
@@ -10,7 +10,7 @@ from lib.obstacle import Obstacle
 
 # MAIN VARIABLES
 GAME = {
-    'AUTHORS'       : ['VIKTOR A. ROZENKO VOITENKO', 'BENJAMIN A. MEDHURST', 
+    'AUTHORS'       : ['VIKTOR A. ROZENKO VOITENKO', 'BENJAMIN A. MEDHURST',
                        'TUNG DO VIET', 'HENRIQUE CRUZ FONSECA'],
     'VERSION'       : 'ALPHA',
     'FRAME RATE'    : 25,
@@ -25,14 +25,21 @@ GAME = {
 flappy = Bird()
 obstacles = list()
 
-master = Tk()
-canvas = Canvas(master, width=GAME['WIDTH'], height=GAME['HEIGHT'])
+master = Tkinter.Tk()
+
+canvas = Tkinter.Canvas(master, width=GAME['WIDTH'], height=GAME['HEIGHT'])
 canvas.pack()
-circle = canvas.create_oval(10, 10, 60, 60, fill='red')
-canvas.pack()
-delay(5)
+
+x = 0
+
+def repeat_me():
+    global x
+    canvas.delete(Tkinter.ALL)
+    canvas.create_rectangle(x,50,x+50,50+50, fill = "yellow")
+    x = x + 10
+    master.after(1000, repeat_me)
+
+
+repeat_me()
+
 master.mainloop()
-delay(3)
-canvas.move(circle, 50, 50)
-canvas.update()
-master.update()
