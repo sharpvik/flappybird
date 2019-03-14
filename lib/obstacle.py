28555cdb779f1185f467a887c3981f784f8191be
Viktor Rozenko <sharp.vik@gmail.com>
2019-03-14 01:24:56 +0000

    obstacle generation improved and completed

--- lib/obstacle.py
+++ lib/obstacle.py
@@ -6,26 +6,16 @@ class Obstacle:
         self.y = y
         self.height = height
         self.ground = height / 10 * 8
-        self.spriteheads = Tkinter.PhotoImage(file='img/obstacle.gif')
-        self.spritecoline = Tkinter.PhotoImage(file='img/coline.gif')
+        self.sprite = Tkinter.PhotoImage(file='img/obstacle.gif')
         self.framerate = framerate
         self.speed = speed
+        self.replaced = False
         
     def logic(self):
         self.x -= self.speed / float(self.framerate)
         
     def render(self, canvas):
         # render the gates
-        canvas.create_image(self.x, self.y, image=self.spriteheads)
-        # render the lines
-            # render up
-        y = self.y - 123
-        while y >= 0:
-            canvas.create_image(self.x, y, image=self.spritecoline)
-            y -= 3
-            # render down
-        y = self.y + 123
-        while y <= self.ground:
-            canvas.create_image(self.x, y, image=self.spritecoline)
-            y += 3
+        # gates are 243px high
+        canvas.create_image(self.x, self.y, image=self.sprite)
         self.logic()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # STANDARD LIBRARY IMPORTS
 import Tkinter
+from random import randint
 
 # OWN MODULES IMPORTS
 from lib.background import Background
+from lib.ground import Ground
 from lib.bird import Bird
 from lib.obstacle import Obstacle
 
@@ -10,40 +12,56 @@ from lib.obstacle import Obstacle
 
 # MAIN VARIABLES
 GAME = {
-    'AUTHORS'       : ['VIKTOR A. ROZENKO VOITENKO', 'BENJAMIN A. MEDHURST',
-                       'TUNG DO VIET', 'HENRIQUE CRUZ FONSECA'],
-    'VERSION'       : 'ALPHA',
-    'ON'            : False,
-    'OVER'          : False,
-    'FRAME RATE'    : 50, # frames/second
-    'DELAY'         : 1000 / 50, # milliseconds
-    'WIDTH'         : 990, # pixels
-    'HEIGHT'        : 600, # pixels
-    'BIRD X'        : 200, # pixels
-    'BIRD Y'        : 240, # pixels
-    'BIRD SIZE'     : 30, # pixels
-    'BIRD VELOCITY' : 0, # pixels/second
-    'BIRD SPEED'    : 350, # pixels/second
-    'BIRD G'        : 70, # pixels/second^2
+    'AUTHOR'            : 'VIKTOR A. ROZENKO VOITENKO',
+    'VERSION'           : 'ALPHA',
+    'ON'                : False,
+    'OVER'              : False,
+    'FRAME RATE'        : 50, # frames/second
+    'DELAY'             : 1000 / 50, # milliseconds
+    'WIDTH'             : 990, # pixels
+    'HEIGHT'            : 600, # pixels
+    'BIRD X'            : 200, # pixels
+    'BIRD Y'            : 240, # pixels
+    'BIRD SIZE'         : 30, # pixels
+    'BIRD VELOCITY'     : 0, # pixels/second
+    'BIRD SPEED'        : 350, # pixels/second
+    'BIRD G'            : 66, # pixels/second^2
+    'OBSTACLE SPACING'  : 300, # pixels
 }
 
 
 
 # MAIN FUNCTIONS
 def keypress_redirect(e):
-    if e.char == ' ':           # START / FLAP
+    key = e.char.lower()
+    if key == ' ':           # START / FLAP
         if not GAME['ON']:
             GAME['ON'] = True
             gameloop()
         flappy.flap()
-    elif e.char == 'p':         # PAUSE
+    elif key == 'p':         # PAUSE
         GAME['ON'] = False
-    elif e.char == 'r':         # RESUME
+    elif key == 'r':         # RESUME
         if not GAME['ON']:
             GAME['ON'] = True
             gameloop()
-    elif e.char == 'q':         # QUIT
+    elif key == 'q':         # QUIT
         quit()
+        
+        
+def logic():
+    global GAME, obstacles
+    # destroy invisible obstacle
+    if obstacles[0].x < -50:
+        obstacles.pop(0)
+    # create new obstacle when needed
+    if obstacles[-1].x <= GAME['WIDTH'] and not obstacles[-1].replaced:
+        updated_x = obstacles[-1].x + GAME['OBSTACLE SPACING']
+        random_y  = randint(132, 350)
+        obstacles.append(
+            Obstacle(updated_x, random_y, GAME['HEIGHT'], GAME['FRAME RATE'], 
+                     GAME['BIRD SPEED'])
+        )
 
 
 
@@ -53,16 +71,15 @@ master = Tkinter.Tk()
 
 
     # OBJECTS
-background = Background(GAME['WIDTH'], GAME['HEIGHT'], GAME['BIRD SPEED'],
+background = Background(GAME['WIDTH'], GAME['HEIGHT'])
+ground = Ground(GAME['WIDTH'], GAME['HEIGHT'], GAME['BIRD SPEED'],
                         GAME['FRAME RATE'])
 flappy = Bird(GAME['BIRD X'], GAME['BIRD Y'], GAME['BIRD SIZE'], 
               GAME['BIRD VELOCITY'], GAME['BIRD G'], 
               GAME['FRAME RATE'], GAME['HEIGHT'])
 obstacles = [
-    Obstacle(400, 350, GAME['HEIGHT'], GAME['FRAME RATE'], GAME['BIRD SPEED']),
-    Obstacle(700, 350, GAME['HEIGHT'], GAME['FRAME RATE'], GAME['BIRD SPEED']),
-    Obstacle(1000, 350, GAME['HEIGHT'], GAME['FRAME RATE'], GAME['BIRD SPEED']),
-    Obstacle(1300, 350, GAME['HEIGHT'], GAME['FRAME RATE'], GAME['BIRD SPEED'])
+    Obstacle(GAME['WIDTH'] + 100, randint(132, 350), GAME['HEIGHT'], 
+             GAME['FRAME RATE'], GAME['BIRD SPEED']),
 ]
 
 
@@ -78,6 +95,8 @@ canvas.pack()
 
 
 def gameloop():
+    logic()
+    
     canvas.delete(Tkinter.ALL) # clear canvas
     
     # render stuff
@@ -85,6 +104,7 @@ def gameloop():
     flappy.render(canvas)
     for obstacle in obstacles:
         obstacle.render(canvas)
+    ground.render(canvas)
     
     if GAME['ON']: # if GAME is not PAUSED
         master.after(GAME['DELAY'], gameloop) # continue gameloop
